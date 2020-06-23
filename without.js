6bbd64b3a574b9b56c844d71aff667bda223c3eb
Françis <francispellerinvenne@hotmail.fr>
2020-06-23 21:46:52 +0000

    keeps only the unique values

---
+++ without.js
@@ -0,0 +1,23 @@
+/* let eqArrays = function(firstArray, secondArray) {
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray[i] !== secondArray[i]) {
+      console.log(`🛑🛑🛑 Assertion Failed: "${firstArray}" !== "${secondArray}"`);
+    } else if (firstArray.length !== secondArray.length) {
+      console.log(`🛑🛑🛑 Assertion Failed: "${firstArray}" !== "${secondArray}"`);
+  }
+    }
+    return`✅✅✅ Assertion Passed: ${firstArray} === ${secondArray}`;
+} */
+
+let without = function(firstArray, secondArray) {
+  let removedElements = [];
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray[i] !== secondArray[i]) {
+      removedElements.push(firstArray[i]);
+    }
+  }
+  return removedElements
+}
+
+console.log(without([1, 2, 3], [1])) // => [2, 3]
+console.log(without(["1", "2", "3"], [1, 2, "3"]))
