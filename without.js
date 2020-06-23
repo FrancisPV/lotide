b9428a9c2ce43922a4454326e2ca7870217560e9
Françis <francispellerinvenne@hotmail.fr>
2020-06-23 21:48:17 +0000

    ..

--- without.js
+++ without.js
@@ -7,6 +7,17 @@
   }
     }
     return`✅✅✅ Assertion Passed: ${firstArray} === ${secondArray}`;
+}
+
+let eqArrays = function(firstArray, secondArray) {
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray[i] !== secondArray[i]) {
+      console.log(`🛑🛑🛑 Assertion Failed: "${firstArray}" !== "${secondArray}"`);
+    } else if (firstArray.length !== secondArray.length) {
+      console.log(`🛑🛑🛑 Assertion Failed: "${firstArray}" !== "${secondArray}"`);
+  }
+    }
+    return`✅✅✅ Assertion Passed: ${firstArray} === ${secondArray}`;
 } */
 
 let without = function(firstArray, secondArray) {
