8d39b55449adeae9ece97314c7951dca3a01857d
Françis <francispellerinvenne@hotmail.fr>
2020-06-29 15:50:35 +0000

    refactoring od eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,13 +1,3 @@
-/* const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: "${actual}" !== "${expected}"`);
-  }
-};
- */
-
-
 let eqArrays = function(firstArray, secondArray) {
   for (let i = 0; i < firstArray.length; i++) {
     if (firstArray[i] !== secondArray[i]) {
@@ -18,10 +8,5 @@ let eqArrays = function(firstArray, secondArray) {
   }
   return true
 }
+module.exports = eqArrays;
 
-
-
-console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
-console.log(eqArrays([1, 1, 3], [1, 2, 3])) // => false
-console.log(eqArrays([1, 2, 3], [1, 2, 3, 4])) // => false
-console.log(eqArrays([1, 2, 3], [3, 2, 1]))// => false
