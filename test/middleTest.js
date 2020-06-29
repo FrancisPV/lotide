d87b9b401a20a415e5ad21684181bb8581a339ba
Françis <francispellerinvenne@hotmail.fr>
2020-06-29 17:26:20 +0000

    refactoring middle and tail with mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,14 +1,20 @@
-const assertEqual = require('../assertEqual');
-const assertArrayEquals = require('../assertArrayEquals');
-const middle = require('../middle');
-const eqArrays = require('../eqArrays');
+const assert = require('chai').assert;
+const middle   = require('../middle');
 
-assertArrayEquals(middle([1]), [1, 2, 3, 4]) // => []
-assertArrayEquals(middle([1, 2]), []) // => []
-assertArrayEquals(middle([1, 2, 3]), [2])// => [2]
-assertArrayEquals(middle([1, 2, 3, 4, 5]), [3]) // => [3]
-assertArrayEquals(middle([1, 2, 3, 4]), [2, 3]) // => [2, 3]
-assertArrayEquals(middle([1, 2, 3, 4, 5, 6]), [3, 4]) // => [3, 4]
+describe("#middle", () => {
+  it("returns an empty array when giving an array of 1 number", () => {
+    assert.isEmpty(middle([1]), []);
+  });
+  it("returns an empty array when giving an array of two numbers", ()  => {
+    assert.isEmpty(middle([1, 2]), []); 
+  }); 
+  it("returns 2 when passing [1, 2, 3]", () => {
+    assert.equal(middle([1, 2, 3]), 2)
+  })
+  it("returns [2, 3] when passing [1, 2, 3, 4]", () => {
+    assert.equal[middle([1, 2, 3, 4]), [2, 3]]
+  });
+});
 
-// '' === '' -> true
-// [] === [] -> false
+
+
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,8 +1,20 @@
-const assertEqual = require('../assertEqual');
+/* const assertEqual = require('../assertEqual');
+const tail = require('../tail'); */
+
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-const result = tail(["Hello"]);
+describe("#middle", () => {
+  it("returns an empty array when giving an array of 1 element", () => {
+    assert.isEmpty(tail([1]), []);
+  });
+  it("returns [1, 2] when passing [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [1, 2])
+  });
+});
+
+/* const result = tail(["Hello"]);
 const words = ['dog'];
 tail(words);
 assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
-assertEqual(words.length, 0); // original array should still have 3 elements!
+assertEqual(words.length, 0); // original array should still have 3 elements! */
